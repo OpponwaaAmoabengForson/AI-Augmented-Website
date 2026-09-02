e2fd0c58cb9b794fa8bf47aa816cf2b6aee1949c
Opponwaa Amoabeng Forson <amoabengopponwaaforson4@gmail.com>
2026-09-02 17:09:21 +0000

    Added a dropdown menu for smaller screen

--- script.js
+++ script.js
@@ -94,3 +94,18 @@ window.addEventListener("load", () => {
         imgPreloader.src = imageSrc;
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const menuToggle = document.getElementById("menuToggle");
+    const navLinks = document.getElementById("navLinks");
+
+    if (menuToggle && navLinks) {
+
+        menuToggle.addEventListener("click", function () {
+            navLinks.classList.toggle("active");
+        });
+
+    }
+
+});
